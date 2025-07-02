79776eaedd9d30ae53fe13d2fad2337d2a76f944
Kent Michael San <kentmichaelsan1997@gmail.com>
2025-07-02 23:23:29 +0800

    feat: add profile picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import HumburgerMenu from "./components/humburger_menu";
 
 export default function Home() {
   return (
-    <main className="relative py-10 px-5">
-      <HumburgerMenu />
-      <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-        Kent Michael E. San
-      </code>
-    </main>
+    <>
+      <main className="py-10 px-5">
+        <HumburgerMenu />
+        <div className="h-[600px] mt-7 rounded-tr-[50%] rounded-tl-2xl rounded-bl-[50%] rounded-br-2xl bg-[url(/images/profile.jpeg)] bg-cover bg-top"></div>
+      </main>
+    </>
   );
 }
