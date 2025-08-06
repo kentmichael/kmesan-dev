9a09baf3117ca3a4eb730547ff7e18e39c056986
Kent Michael San <kentmichaelsan1997@gmail.com>
2025-08-06 23:10:44 +0800

    fix: my projects section description

--- src/app/components/my_projects.tsx
+++ src/app/components/my_projects.tsx
@@ -4,8 +4,7 @@ export default function MyProjects() {
       <h1 className="text-2xl font-semibold text-[#3ad353]">My Projects</h1>
       <p>
         Here&apos;s a look at some of the stuff I&apos;ve been working on
-        lately. These projects show what I can do with web development, design,
-        and solving real-world problems.
+        lately.
       </p>
     </div>
   );
